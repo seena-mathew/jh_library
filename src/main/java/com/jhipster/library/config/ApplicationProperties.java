c5572d94eba80f257f400ad909f73eca67639075
seena-mathew <seenamathew078@gmail.com>
2021-05-02 19:32:30 +0530

    Initial version of jh_library generated by JHipster-7.0.1

---
+++ src/main/java/com/jhipster/library/config/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package com.jhipster.library.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Jh Library.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {}
---
+++ src/main/java/com/jhipster/library/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.jhipster.library.repository;
+
+import com.jhipster.library.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
